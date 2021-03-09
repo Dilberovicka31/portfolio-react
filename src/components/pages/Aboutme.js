470ef7ca1f7c3d478688af67c9be5d37d3eb37da
Dilberovicka31 <dilberovicm36@gmail.com>
2021-03-08 16:18:59 -0800

    fixed work and contact page

--- src/components/pages/Aboutme.js
+++ src/components/pages/Aboutme.js
@@ -7,21 +7,21 @@ function Aboutme() {
       <MDBCol md="4" className="rounded mx-auto d-block">
         <img
           src={profileimg}
-          class="img-fluid z-depth-5 r"
+          class="img-fluid z-depth-5 animated slideInRight"
           alt="Mia Dilberovic Profile"
         ></img>
       </MDBCol>
       <MDBCol className="mx-auto" md="6">
-        <p className="font-weight-bolder"> Hi I'm Mia.</p>
+        <p className="font-weight-bolder animated slideInUp" style={{fontSize: '22px'}}> Hi I'm Mia.</p>
 
-        <p className="font-weight-bolder">
+        <p className="font-weight-bolder animated slideInUp" style={{fontSize: '22px'}}>
           I'm a full stack web developer experienced in HTML, CSS, JavaScript,
           MySQL, MongoDB, Sequelize, to name a few. I have experience in many
           industries, which I'm grateful for as all of them have provided me
           with great soft skills and a lot of experience.
         </p>
 
-        <p className="font-weight-bolder">
+        <p className="font-weight-bolder animated slideInUp" style={{fontSize: '22px'}}>
           Creating art and designing is my passion and it gives me gratification
           constantly. Even when that gratification is delayed after struggling
           for hours to figure out why something isn't working.
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Contact() {
 
-    return(
 
+function Contact () {
+    
+    
+   
+      return (
         <div></div>
-    )
-}
+        );
+      }
+    
+    
 export default Contact;
